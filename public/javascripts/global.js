8fb51107e88acc9b48f047c0d0a7929ce83b09e2
Charvel <eduardocharvel@cis.neustar.com>
2018-12-07 12:33:04 -0800

    update user

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     populateTable();
     $("#userList table tbody").on("click","td a.linkshowuser", showUserInfo);
     $('#btnAddUser').on('click',addUser);
+    $('#btnUpdateUser').on('click',updateUser);
+    $('#userList table tbody').on('click','td a.linkdeleteuser', deleteUser);
 });
 
 function populateTable() {
@@ -11,7 +13,7 @@ function populateTable() {
         userListData = data;
         $.each(data,function() {
             tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">'+this.username+'</a></td>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this._id + '">'+this.username+'</a></td>';
             tableContent += '<td>'+this.email+'</td>' ;
             tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
@@ -22,17 +24,27 @@ function populateTable() {
 
 function showUserInfo(event) {
     event.preventDefault();
-    var thisUserName = $(this).attr('rel');
+    var thisId = $(this).attr('rel');
     var arrayPosition = userListData.map(function(arrayItem) {
-        return arrayItem.username;
-    } ).indexOf(thisUserName);    
+        return arrayItem._id;
+    } ).indexOf(thisId);    
 
     var thisUserObject = userListData[arrayPosition];
 
+    
     $("#userInfoName").text(thisUserObject.fullname);
     $("#userInfoAge").text(thisUserObject.age);
     $("#userInfoGender").text(thisUserObject.gender);
     $("#userInfoLocation").text(thisUserObject.location);
+
+    $('#addUser fieldset input#inputUserId').val(thisUserObject._id);
+    $('#addUser fieldset input#inputUserName').val(thisUserObject.username);
+    $('#addUser fieldset input#inputUserEmail').val(thisUserObject.email);
+    $('#addUser fieldset input#inputUserFullname').val(thisUserObject.fullname);
+    $('#addUser fieldset input#inputUserAge').val(thisUserObject.age);
+    $('#addUser fieldset input#inputUserLocation').val(thisUserObject.location);
+    $('#addUser fieldset input#inputUserGender').val(thisUserObject.gender);
+
 }
 
 function addUser(event) {
@@ -41,35 +53,95 @@ function addUser(event) {
     var errorCount = 0;
     $('#addUser input').each( function(index,val) {
         if ($(this).val() === '') {errorCount++;}
+    });
 
-        if (errorCount === 0 ) {
-            var newUser = {
-                'username':$('#addUser fieldset input#inputUserName').val(),
-                'email':$('#addUser fieldset input#inputUserEmail').val(),
-                'fullname':$('#addUser fieldset input#inputUserFullname').val(),
-                'age':$('#addUser fieldset input#inputUserAge').val(),
-                'location':$('#addUser fieldset input#inputUserLocation').val(),
-                'gender':$('#addUser fieldset input#inputUserGender').val(),
-            } ;
+    if (errorCount === 0 ) {
+        var newUser = {
+            'username':$('#addUser fieldset input#inputUserName').val(),
+            'email':$('#addUser fieldset input#inputUserEmail').val(),
+            'fullname':$('#addUser fieldset input#inputUserFullname').val(),
+            'age':$('#addUser fieldset input#inputUserAge').val(),
+            'location':$('#addUser fieldset input#inputUserLocation').val(),
+            'gender':$('#addUser fieldset input#inputUserGender').val(),
+        } ;
 
-            $.ajax({
-                type:'POST',
-                data:newUser,
-                url: '/users/adduser',
-                dataType: 'JSON'
-            }).done(function(response) {
-                if (response.msg === '') {
-                    ('#addUser fieldset input').val('');
-                    populateTable();
-                }
-                else {
-                    alert('Error: ' + response.msg);
-                }
-            });
-        }
-        else {
-            alert('Please fill in all fields');
-            return false;
-        }
-    })
+        $.ajax({
+            type:'POST',
+            data:newUser,
+            url: '/users/adduser',
+            dataType: 'JSON'
+        }).done(function(response) {
+            if (response.msg === '') {
+                $('#addUser fieldset input').val('');
+                populateTable();
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+        });
+    }
+    else {
+        alert('Please fill in all fields');
+        return false;
+    }
+};
+
+function updateUser(event) {
+    event.preventDefault();
+    var errorCount = 0;
+    $('#addUser input').each( function(index,val) {
+        if ($(this).val() === '') {errorCount++;}
+    });
+
+    if (errorCount === 0 ) {
+        var newUser = {
+            '_id':$('#addUser fieldset input#inputUserId').val(),
+            'username':$('#addUser fieldset input#inputUserName').val(),
+            'email':$('#addUser fieldset input#inputUserEmail').val(),
+            'fullname':$('#addUser fieldset input#inputUserFullname').val(),
+            'age':$('#addUser fieldset input#inputUserAge').val(),
+            'location':$('#addUser fieldset input#inputUserLocation').val(),
+            'gender':$('#addUser fieldset input#inputUserGender').val(),
+        } ;
+
+        $.ajax ( {
+            type:'PUT',
+            url:'/users/updateuser/',
+            data:newUser,
+            dataType:'JSON'
+        }).done(function(response) {
+            if (response.msg === '') {
+                $('#addUser fieldset input').val('');
+                populateTable();
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+        });
+    }
+}
+
+
+function deleteUser(event) {
+    event.preventDefault();
+
+    var confirmation = confirm('Are you sure you want to delte this user?');
+
+    if (confirmation === true) {
+        $.ajax( {
+            type:'DELETE',
+            url:'/users/deleteuser/' + $(this).attr('rel')
+        }).done(function(response) {
+            if (response.msg === ''  ) {
+
+            }
+            else {
+                alert('Error:' + response.msg);
+            }
+            populateTable();
+        });
+    }
+    else {
+        return false;
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,48 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/userlist', function(req,res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    console.log('got here');
+    collection.find({},{},function(e,docs) {
+        console.log(docs);
+        res.json(docs);
+    });
+    console.log('got here too');
+});
+
+router.put('/updateuser', function(req,res) {
+    console.log('edit user called');
+    var db = req.db;
+    var collection = db.get('userlist');
+    console.log('updateing user to ');
+    console.log(req.body);
+    collection.update({ "_id": req.body._id}, req.body ,function(err,result) {
+        res.send(
+            (err === null)?{msg:''}:{msg:err}
+        );
+    });
+});
+
+router.post('/adduser', function(req,res) {
+    console.log('add user called');
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body,function(err,result) {
+        res.send(
+            (err === null)?{msg:''}:{msg:err}
+        );
+    });
+});
+
+router.delete('/deleteuser/:id',function( req,res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({'_id': userToDelete},function(err) {
+        res.send((err === null) ? {msg:''}:{msg:'error:' + err});
+    });
+});
+
 module.exports = router;
